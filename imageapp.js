332380c1be7524b85575e5b9b0b9a1e1e4357b30
Carlie-Dev <ellepheonixgames@gmail.com>
2024-02-12 13:22:39 -0600

    moved fuctionality to JS

--- imageapp.js
+++ imageapp.js
@@ -1,13 +1,51 @@
+
 /**
  * 
- * @param {object} skininput
- * @returns {} -void
- * @example
- * 
+ * @param {object} ev  
+ * @returns {} - void
+ *
  */
+function addImage(ev){
+    console.log(ev.target.id + ' was clicked');
+    
 
-function imagechanger(skininput){
-    document.querySelector('#mainImage').src = skininput.src;
-    //For console reference
-    console.log(skininput);
+    let myImage = document.createElement("img");
+    myImage.src = "images/" + ev.target.src;
+    myImage.alt = "This is a minecraft skin"
+
+    document.querySelector('#mainImage').src = ev.target.src;
+
 }
+
+//These are the event listeners
+document.getElementById('ellephoenix').addEventListener('click', addImage);
+
+document.getElementById('mobster').addEventListener('click', addImage);
+
+document.getElementById('ice_king').addEventListener('click', addImage);
+
+document.getElementById('woman').addEventListener('click', addImage);
+
+
+
+
+//Main image code from lesson
+// document.getElementById('mainImage').addEventListener('click',function(ev){
+
+//     console.log(ev.target.id);
+//     console.log('Was clicked');
+
+//     let myImage = document.createElement("img");
+//     myImage.src = "images/" + ev.target.src;
+//     myImage.alt = "This is the main image"
+
+//     document.querySelector('#mainImage').src = ev.target.src;
+    
+// })
+
+//Former Code
+// function imagechanger(skininput){
+//     document.querySelector('#mainImage').src = skininput.src;
+//     //For console reference
+//     console.log(skininput);
+// } 
